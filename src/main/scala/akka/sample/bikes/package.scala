0707be77303f2b559856d64f2c1480411dd6ce40
Massimo Calderoni <max.calderoni@gmail.com>
2020-03-25 06:51:54 -0700

    Remove the `read()` part of json formatters (unused).

--- src/main/scala/akka/sample/bikes/package.scala
+++ src/main/scala/akka/sample/bikes/package.scala
@@ -43,14 +43,8 @@ package object bikes {
     implicit val blueprintFormat = jsonFormat4(Blueprint)
     implicit val inventoryFormat = jsonFormat1(Inventory)
 
-    implicit object InitStateJsonFormat extends JsonFormat[Bike.InitState.type] {
+    implicit object InitStateJsonFormat extends JsonWriter[Bike.InitState.type] {
       def write(c: Bike.InitState.type) = JsObject("state" -> JsString("init"))
-      def read(value: JsValue): Bike.InitState.type = {
-        value.asJsObject.getFields("state") match {
-          case Seq(JsString(_)) => Bike.InitState
-          case _ => throw new DeserializationException("InitState expected")
-        }
-      }
     }
     implicit val downloadingFormat = jsonFormat1(Bike.DownloadingState)
     implicit val downloadedFormat = jsonFormat1(Bike.DownloadedState)
@@ -59,76 +53,49 @@ package object bikes {
     implicit val reservingFormat = jsonFormat2(Bike.ReservingState)
     implicit val yieldingFormat = jsonFormat2(Bike.YieldingState)
 
-    implicit object ErrorStateJsonFormat extends JsonFormat[Bike.ErrorState] {
+    implicit object ErrorStateJsonFormat extends JsonWriter[Bike.ErrorState] {
       def write(c: Bike.ErrorState) = JsObject(
         "name" -> JsString("ErrorState"),
         "msg" -> JsString(c.msg),
         "offendingCommand" -> JsString(c.offendingCommand.getClass.getSimpleName),
         "lastState" -> JsString(c.lastState.getClass.getSimpleName))
-      def read(value: JsValue): Bike.ErrorState = {
-        value.asJsObject.getFields("msg", "offendingCommand", "lastState") match {
-          case Seq(JsString(msg), JsString(_), JsString(_)) => Bike.ErrorState(msg, Bike.KickCmd, Bike.InitState)
-          case _ => throw new DeserializationException("ErrorState expected")
-        }
-      }
     }
 
-    implicit object ReservedStateJsonFormat extends JsonFormat[Bike.ReservedState] {
+    implicit object ReservedStateJsonFormat extends JsonWriter[Bike.ReservedState] {
       def write(c: Bike.ReservedState) = JsObject(
         "name" -> JsString("ReservedState"),
         "location" -> c.location.toJson)
-      def read(value: JsValue): Bike.ReservedState = {
-        value.asJsObject.getFields("blueprint", "location") match {
-          case Seq(blueprint, location) => Bike.ReservedState(blueprint.convertTo[Bike.Blueprint], location.convertTo[Bike.NiUri])
-          case _ => throw DeserializationException("ReservedState expected")
-        }
-      }
     }
 
-    implicit object YieldedStateJsonFormat extends JsonFormat[Bike.YieldedState] {
+    implicit object YieldedStateJsonFormat extends JsonWriter[Bike.YieldedState] {
       def write(c: Bike.YieldedState) = JsObject(
         "name" -> JsString("YieldedState"),
         "location" -> c.location.toJson)
-      def read(value: JsValue): Bike.YieldedState = {
-        value.asJsObject.getFields("blueprint", "location") match {
-          case Seq(blueprint, location) => Bike.YieldedState(blueprint.convertTo[Bike.Blueprint], location.convertTo[Bike.NiUri])
-          case _ => throw DeserializationException("YieldedState expected")
-        }
-      }
     }
 
-    implicit object StateFormat extends RootJsonFormat[Bike.State] {
+    implicit object StateFormat extends RootJsonWriter[Bike.State] {
       def write(obj: Bike.State): JsValue =
         JsObject((obj match {
           case Bike.InitState => JsObject("name" -> JsString("InitState"))
-          case c: Bike.DownloadingState => JsObject("name" -> JsString("DownloadingState"))
-          case c: Bike.DownloadedState => JsObject("name" -> JsString("DownloadedState"))
-          case c: Bike.CreatingState => JsObject("name" -> JsString("CreatingState"))
-          case c: Bike.CreatedState => JsObject("name" -> JsString("CreatedState"))
-          case c: Bike.ReservingState => JsObject("name" -> JsString("ReservingState"))
-          case c: Bike.ReservedState => c.toJson
-          case c: Bike.YieldingState => JsObject("name" -> JsString("YieldingState"))
-          case c: Bike.YieldedState => c.toJson
-          case a: Bike.ErrorState => a.toJson
+          case _: Bike.DownloadingState => JsObject("name" -> JsString("DownloadingState"))
+          case _: Bike.DownloadedState => JsObject("name" -> JsString("DownloadedState"))
+          case _: Bike.CreatingState => JsObject("name" -> JsString("CreatingState"))
+          case _: Bike.CreatedState => JsObject("name" -> JsString("CreatedState"))
+          case _: Bike.ReservingState => JsObject("name" -> JsString("ReservingState"))
+          case state: Bike.ReservedState => state.toJson
+          case _: Bike.YieldingState => JsObject("name" -> JsString("YieldingState"))
+          case state: Bike.YieldedState => state.toJson
+          case state: Bike.ErrorState => state.toJson
           case unknown => deserializationError(s"json deserialize error: $unknown")
         }).asJsObject.fields)
 
-      def read(json: JsValue): Bike.State =
-        json.asJsObject.getFields("name") match {
-          case Seq(JsString("DownloadingState")) => json.convertTo[Bike.DownloadingState]
-          case Seq(JsString("DownloadedState")) => json.convertTo[Bike.DownloadedState]
-          case Seq(JsString("CreatingState")) => json.convertTo[Bike.CreatingState]
-          case Seq(JsString("CreatedState")) => json.convertTo[Bike.CreatedState]
-          case Seq(JsString("ReservingState")) => json.convertTo[Bike.ReservingState]
-          case Seq(JsString("ReservedState")) => json.convertTo[Bike.ReservedState]
-          case Seq(JsString("YieldingState")) => json.convertTo[Bike.YieldingState]
-          case Seq(JsString("YieldedState")) => json.convertTo[Bike.YieldedState]
-          case Seq(JsString("ErrorState")) => json.convertTo[Bike.ErrorState]
-          case unrecognized => serializationError(s"json serialization error $unrecognized")
-        }
     }
 
-    implicit val queryStatusFormat = jsonFormat2(BikeRoutes.QueryStatus)
+    implicit object QueryStatusFormat extends RootJsonWriter[BikeRoutes.QueryStatus] {
+      def write(c: BikeRoutes.QueryStatus) = JsObject(
+        "bikeId" -> JsString(c.bikeId),
+        "state" -> c.state.toJson)
+    }
 
   }
 }
