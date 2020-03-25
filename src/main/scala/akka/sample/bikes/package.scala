eaab6348e3e9bf13f9d947c429466c793a66b64c
Massimo Calderoni <max.calderoni@gmail.com>
2020-03-25 05:07:34 -0700

    Json support for `QueryStatus`.

--- src/main/scala/akka/sample/bikes/package.scala
+++ src/main/scala/akka/sample/bikes/package.scala
@@ -1,5 +1,8 @@
 package akka.sample
 
+import spray.json.{ DeserializationException, JsObject, JsString, JsValue, JsonFormat, RootJsonFormat }
+import spray.json._
+
 package object bikes {
   import akka.cluster.sharding.typed.{ ShardingEnvelope, ShardingMessageExtractor }
 
@@ -39,5 +42,70 @@ package object bikes {
     implicit val coordsFormat = jsonFormat2(NiUri)
     implicit val blueprintFormat = jsonFormat4(Blueprint)
     implicit val inventoryFormat = jsonFormat1(Inventory)
+
+    implicit object InitStateJsonFormat extends JsonFormat[Bike.InitState.type] {
+      def write(c: Bike.InitState.type) = JsObject("state" -> JsString("init"))
+      def read(value: JsValue): Bike.InitState.type = {
+        value.asJsObject.getFields("state") match {
+          case Seq(JsString(_)) => Bike.InitState
+          case _ => throw new DeserializationException("InitState expected")
+        }
+      }
+    }
+    implicit val downloadingFormat = jsonFormat1(Bike.DownloadingState)
+    implicit val downloadedFormat = jsonFormat1(Bike.DownloadedState)
+    implicit val creatingFormat = jsonFormat1(Bike.CreatingState)
+    implicit val createdFormat = jsonFormat2(Bike.CreatedState)
+    implicit val reservingFormat = jsonFormat2(Bike.ReservingState)
+    implicit val reservedFormat = jsonFormat2(Bike.ReservedState)
+    implicit val yieldingFormat = jsonFormat2(Bike.YieldingState)
+    implicit val yieldedFormat = jsonFormat2(Bike.YieldedState)
+
+    implicit object ErrorStateJsonFormat extends JsonFormat[Bike.ErrorState] {
+      def write(c: Bike.ErrorState) = JsObject(
+        "msg" -> JsString(c.msg),
+        "offendingCommand" -> JsString(c.offendingCommand.getClass.getSimpleName),
+        "lastState" -> JsString(c.lastState.getClass.getSimpleName))
+      def read(value: JsValue) = {
+        value.asJsObject.getFields("msg", "offendingCommand", "lastState") match {
+          case Seq(JsString(msg), JsString(_), JsString(_)) => Bike.ErrorState(msg, Bike.KickCmd, Bike.InitState)
+          case _ => throw new DeserializationException("ErrorState expected")
+        }
+      }
+    }
+
+    implicit object StateFormat extends RootJsonFormat[Bike.State] {
+      def write(obj: Bike.State): JsValue =
+        JsObject((obj match {
+          case Bike.InitState => JsString("init")
+          case c: Bike.DownloadingState => c.toJson
+          case c: Bike.DownloadedState => c.toJson
+          case c: Bike.CreatingState => c.toJson
+          case c: Bike.CreatedState => c.toJson
+          case c: Bike.ReservingState => c.toJson
+          case c: Bike.ReservedState => c.toJson
+          case c: Bike.YieldingState => c.toJson
+          case c: Bike.YieldedState => c.toJson
+          case a: Bike.ErrorState => a.toJson
+          case unknown => deserializationError(s"json deserialize error: $unknown")
+        }).asJsObject.fields)
+
+      def read(json: JsValue): Bike.State =
+        json.asJsObject.getFields("name") match {
+          case Seq(JsString("DownloadingState")) => json.convertTo[Bike.DownloadingState]
+          case Seq(JsString("DownloadedState")) => json.convertTo[Bike.DownloadedState]
+          case Seq(JsString("CreatingState")) => json.convertTo[Bike.CreatingState]
+          case Seq(JsString("CreatedState")) => json.convertTo[Bike.CreatedState]
+          case Seq(JsString("ReservingState")) => json.convertTo[Bike.ReservingState]
+          case Seq(JsString("ReservedState")) => json.convertTo[Bike.ReservedState]
+          case Seq(JsString("YieldingState")) => json.convertTo[Bike.YieldingState]
+          case Seq(JsString("YieldedState")) => json.convertTo[Bike.YieldedState]
+          case Seq(JsString("ErrorState")) => json.convertTo[Bike.ErrorState]
+          case unrecognized => serializationError(s"json serialization error $unrecognized")
+        }
+    }
+
+    implicit val queryStatusFormat = jsonFormat2(BikeRoutes.QueryStatus)
+
   }
 }
