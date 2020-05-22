ba2a2f03d534581f196adbf83f0ffb68e8cc814f
Massimo <max.calderoni@gmail.com>
2020-05-21 18:53:50 -0700

    Add seed nodes and Main with args for port numbers.
    Add logback.xml.

--- src/main/scala/akka/sample/bikes/Main.scala
+++ src/main/scala/akka/sample/bikes/Main.scala
@@ -1,14 +1,34 @@
 package akka.sample.bikes
 
+/**
+ * See the README.md for starting each node with sbt.
+ */
 object Main {
 
   def main(args: Array[String]): Unit = {
 
+    import akka.actor.AddressFromURIString
     import akka.actor.typed.ActorSystem
     import akka.actor.typed.scaladsl.Behaviors
-    import com.typesafe.config.ConfigFactory
+    import com.typesafe.config.{ Config, ConfigFactory }
 
-    val rootBehavior = Behaviors.setup[Nothing] { _ => Behaviors.empty }
-    ActorSystem[Nothing](rootBehavior, "BikeService", ConfigFactory.load("application_local.conf"))
+    def startNode(config: Config): Unit = {
+      val rootBehavior = Behaviors.setup[Nothing] { _ => Behaviors.empty }
+      ActorSystem[Nothing](rootBehavior, "BikeService", config)
+    }
+
+    def config(port: Int): Config = ConfigFactory.parseString(s"""akka.remote.artery.canonical.port = $port""").
+      withFallback(ConfigFactory.load("application_local.conf"))
+
+    val seedNodes = akka.japi.Util
+      .immutableSeq(ConfigFactory.load("application_local.conf").getStringList("akka.cluster.seed-nodes"))
+      .flatMap { case AddressFromURIString(s) => s.port }
+
+    val ports = args.headOption match {
+      case Some(port) => Seq(port.toInt)
+      case _ => seedNodes ++ Seq(0)
+    }
+
+    ports.foreach { akkaPort => startNode(config(akkaPort)) }
   }
 }
